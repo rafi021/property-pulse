cd892c52e3b386a2af0b87233fffeacc49b6b624
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-03-22 03:07:53 +0600

    server and client components and router hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+  const pathName = usePathname();
+
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {id} {name} {pathName}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
